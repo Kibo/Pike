27cba819af127f0efbddac5585fb403899d6706e
kibo <tomasjurman@gmail.com>
2013-07-31 21:25:16 +0200

    v0.3.5

---
+++ examples/rpg/data/practice.js
@@ -0,0 +1,21 @@
+var PRACTICE_DATA={
+	drummer:[
+			{difficulty:2, 
+			subject:"Peter's brother", 
+			verb:"To go", 
+			tense:"Past Perfect Continuous", 
+			option:"Affirmative", 
+			object:"To the English language academy",
+			solutions:["Peter's brother had been going to the English language academy.", "abc", "cde"]
+			},
+
+			{difficulty:2, 
+			subject:"Our friends", 
+			verb:"To work", 
+			tense:"Present Perfect", 
+			option:"Interrogative", 
+			object:"Hard this morning",
+			solutions:["Have our friends worked hard this morning?", "cde"]
+			}
+		]	
+};
